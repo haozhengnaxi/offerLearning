d83e88b07215ae8a1968c6008c005e234edb0778
STYssd <Shangty1994@126.com>
2020-03-03 21:50:50 +0800

    '第1题'

--- src/offer/T01_Find.java
+++ src/offer/T01_Find.java
@@ -5,8 +5,19 @@ package offer;
  * 二维数组中的查找
  */
 public class T01_Find {
-    public static void main(String[] args) {
-
-
+    public boolean FindLocation(int target,int[][] array) {
+        if (array == null || array.length == 0)
+            return false;
+        int row = array.length; // 行数
+        int col = array[0].length;  //列数
+        for(int i = 0,j = col -1; i <= row - 1 && j >= 0) {
+            if (array[i][j] == target)
+                return true;
+            else if (array[i][j] > target) {
+                j--;
+            }
+            else i++;
+        }
+        return false;
     }
 }
