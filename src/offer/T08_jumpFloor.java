e2c4e5e00104fa304a4420435a20192b56ef79c5
STYssd <Shangty1994@126.com>
2020-03-08 23:37:57 +0800

    '跳台阶'

---
+++ src/offer/T08_jumpFloor.java
@@ -0,0 +1,23 @@
+package offer;
+
+/**
+ * @Author T.Y.Shang
+ * @Date: 2020/3/8 21:29
+ * @Version 1.0
+ */
+public class T08_jumpFloor (int target) {
+    if (0 == target) {
+        return 0;
+        }
+    else if (target == 1) {
+        return 1;
+        }
+    else if (target == 2) {
+        return 2;
+        }
+    else if (target > 2) {
+        return T8_jumpFloor(target - 1) + T8_jumpFloor(target - 1);
+        }
+    return 0;
+}
+
