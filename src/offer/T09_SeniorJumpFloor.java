068b1ef35c816ee71db1a7cada3b7a020c423602
STYssd <Shangty1994@126.com>
2020-03-08 23:38:43 +0800

    '变态跳台阶'

---
+++ src/offer/T09_SeniorJumpFloor.java
@@ -0,0 +1,21 @@
+package offer;
+
+/**
+ * @Author T.Y.Shang
+ * @Date: 2020/3/8 22:16
+ * @Version 1.0
+ */
+public class T09_SeniorJumpFloor (int target) {
+    if (0 == target || 1 == target) {
+        return 1;
+        }
+    else if (2 == target) {
+        return 2;
+        }
+    // 这明显属于递归问题，可以先写出表达式，凑出递归式。根据递归式进行编程
+    int sum = 0;
+    for (int i = 1; i <= target; i++) {
+        sum += T09_SeniorJumpFloor(target - i);
+        }
+    return sum;
+}
